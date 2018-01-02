c5e6c4f8589142d8651c443def2560fd4ac4090a
Aki Väliaho <akivvaliaho@gmail.com>
2018-01-02 13:53:17 +0000

    Rotation test code

--- src/main/java/com/akivaliaho/threed/Clean3DPoint.java
+++ src/main/java/com/akivaliaho/threed/Clean3DPoint.java
@@ -4,7 +4,7 @@ import javafx.geometry.Point3D;
 import javafx.scene.shape.Shape3D;
 
 public class Clean3DPoint extends Point3D {
-    public Clean3DPoint(Shape3D to) {
-        super(to.getTranslateX(), to.getTranslateY(), to.getTranslateZ());
+    public Clean3DPoint(Shape3D shape) {
+        super(shape.getTranslateX(), shape.getTranslateY(), shape.getTranslateZ());
     }
 }
---
+++ src/test/java/com/akivaliaho/threed/GenericRotatorTest.java
@@ -0,0 +1,39 @@
+package com.akivaliaho.threed;
+
+import javafx.scene.shape.Sphere;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GenericRotatorTest {
+
+    private Sphere to;
+    private Sphere from;
+    private GenericRotator genericRotator;
+    private DirectionalCylinder directionalCylinder;
+
+    @Before
+    public void setUp() throws Exception {
+        to = new Sphere(10, 1);
+        to.setTranslateY(100);
+        to.setTranslateX(100);
+        to.setTranslateZ(100);
+        from = new Sphere(10, 1);
+        directionalCylinder = new DirectionalCylinder(100, 100, from, to);
+        this.genericRotator = new GenericRotator(to, directionalCylinder);
+    }
+
+    @Test
+    public void rotateCylinderTowardsTo() {
+        this.genericRotator.rotateCylinderTowardsTo();
+        assertEquals(directionalCylinder.getTranslateX(), 50, 0.0);
+        assertEquals(directionalCylinder.getTranslateY(), 50, 0.0);
+        assertEquals(directionalCylinder.getTranslateZ(), 50, 0.0);
+        //Check distance matches
+        Clean3DPoint to = new Clean3DPoint(this.to);
+        Clean3DPoint from = new Clean3DPoint(this.from);
+        double distance = to.distance(from);
+        assertEquals(distance, directionalCylinder.getHeight(), 0);
+    }
+}
